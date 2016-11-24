c14cb95aac01fba2c44b34e03d4d90659fb68e88
Tobias Famulla <tobias.famulla@travelping.com>
2016-11-24 13:13:52 +0100

    fix execution line

--- docker/ergw-gtp-c-start.sh
+++ docker/ergw-gtp-c-start.sh
@@ -4,4 +4,4 @@ RELPATH=$(find /opt/rel/ergw-gtp-c-node/releases/ -mindepth 1 -maxdepth 1 -type 
 [ -f /config/ergw-gtp-c-node/sys.config ] && cp /config/ergw-gtp-c-node/sys.config $RELPATH/sys.config
 [ -f /config/ergw-gtp-c-node/vm.args ] && cp /config/ergw-gtp-c-node/vm.args $RELPATH/vm.args
 
-$1
+exec "$@"
